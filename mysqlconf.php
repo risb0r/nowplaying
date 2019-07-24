a4c1b7f9e36f7344433fc48534de421bd1593515
risb0r <michael@risby.id.au>
2019-07-24 15:01:14 +1000

    Add files via upload

---
+++ mysqlconf.php
@@ -0,0 +1,19 @@
+<?php
+
+//put your MySQL Information in the supplied spots
+
+$db = "choonstats" ;
+$pass = "ssssssssssssssssss" ;
+$dbloc = "localhost" ; 
+$table = "winamp" ;
+$user = "root" ;
+
+$link = mysql_connect("$dbloc","$user","$pass");
+
+if (! $link)
+die("Couldn't connect to MySQL");
+
+mysql_select_db($db , $link)
+or die("Couldn't open $db: ".mysql_error());
+
+?>
